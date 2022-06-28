aa7579c070e227184cbd5d9624a1287409612463
pyphilia <pyphilia@gmail.com>
2022-06-28 16:02:00 +0200

    refactor: update package

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # graasp-react-shared-dependencies
+
+To be merged with graasp-cli.
--- mergeGlobal.js
+++ mergeGlobal.js
@@ -1,10 +1,14 @@
-// ref: https://stackoverflow.com/questions/39385269/how-to-create-shared-package-json-for-multiple-npm-repositories
+const fs = require("fs");
 
-const fs = require('fs')
+const localPackageJson = require("path-to-local-project/package.json");
+const sharedPackageJson = require("./package.json");
 
-const localPackageJson = require('../../package.json')
-const sharedPackageJson = require('./package.json')
+Object.assign(
+  localPackageJson.dependencies,
+  sharedPackageJson.globalDependencies
+);
 
-Object.assign(localPackageJson.dependencies, sharedPackageJson.globalDependencies)
-
-fs.writeFileSync('../../package.json', JSON.stringify(localPackageJson, null, 2))
+fs.writeFileSync(
+  "path-to-local-project/package.json",
+  JSON.stringify(localPackageJson, null, 2)
+);
