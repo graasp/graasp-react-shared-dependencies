b6b335ad0d4a6804f8915434bb3430a320555a25
pyphilia <pyphilia@gmail.com>
2022-06-28 15:07:06 +0200

    feat: init dependency groups

---
+++ mergeGlobal.js
@@ -0,0 +1,10 @@
+// ref: https://stackoverflow.com/questions/39385269/how-to-create-shared-package-json-for-multiple-npm-repositories
+
+const fs = require('fs')
+
+const localPackageJson = require('../../package.json')
+const sharedPackageJson = require('./package.json')
+
+Object.assign(localPackageJson.dependencies, sharedPackageJson.globalDependencies)
+
+fs.writeFileSync('../../package.json', JSON.stringify(localPackageJson, null, 2))
